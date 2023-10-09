f8e0056cbf0f76fcae2c4b76ef9b5443ccc6b64e
ivenaccip <A01653722@itesm.mx>
2023-10-09 23:45:41 +0100

    test_2_profiles

--- connection_sql.py
+++ connection_sql.py
@@ -1,4 +1,5 @@
 import sqlite3
+import os
 
 def connect_db(db_name):
     # Conectarse a la base de datos (la crea si no existe)
@@ -11,25 +12,31 @@ def connect_db(db_name):
         id INTEGER PRIMARY KEY,
         link TEXT,
         text TEXT,
-        date TEXT
+        date TEXT,
+        profile TEXT
     )
     ''')
     conn.commit()
     
     return conn, cursor
 
-def insert_into_db(conn, cursor, data):
-    cursor.execute("INSERT INTO posts (link, text, date) VALUES (?, ?, ?)", data)
+def insert_into_db(conn, cursor, data, profile_name):
+    cursor.execute("INSERT INTO posts (link, text, date, profile) VALUES (?, ?, ?, ?)", (*data, profile_name))
     conn.commit()
 
 # Usando las funciones
 conn, cursor = connect_db('data_profiles.db')
 
-with open("coffeewithalyss_clean.txt", "r") as file:
-    for line in file.readlines():
-        data = line.strip().split(';')
-        if len(data) == 3:
-            insert_into_db(conn, cursor, data)
+# Lista todos los archivos en el directorio actual con extensión .txt
+txt_files = [f for f in os.listdir('.') if f.endswith('.txt') and f != 'requirements.txt']
+
+for txt_file in txt_files:
+    profile_name = txt_file.replace("_clean.txt", "")
+    with open(txt_file, "r") as file:
+        for line in file.readlines():
+            data = line.strip().split(';')
+            if len(data) == 3:
+                insert_into_db(conn, cursor, data, profile_name)
 
 # Cerrar la conexión
 conn.close()
