1a2dd1aae9aa440d2a1463f26087b5475727ab66
Ivenaccip <ivenaccip@gmail.com>
2024-01-05 11:29:43 -0500

    Ready the planation of UI

--- kroon.sh
+++ kroon.sh
@@ -28,33 +28,59 @@ show_boxes() {
     echo
 }
 
-# Verify Database
-check_database() {
-    db_name="$1"
-    echo "Verifing if the data base exist: $db_name"
-
-    # Comando para verificar la existencia de la base de datos
-    # Asegúrate de reemplazar las credenciales con las tuyas
-    mysql_on "$db_name"
-    if mysql -u "your_mysql_username" -p"your_mysql_password" -e "use $db_name;" 2>/dev/null; then
-        echo "The data base '$db_name' is ready."
+# Check if MySQL is activate
+mysql_on() {
+    if systemctl is-active --quiet mysql; then
+        echo "MySQL service is active."
     else
-        echo "The data base '$db_name' wasn't found." 
-        echo "Do you want to create a database?"
-        read -p " [ C ] Yes | [ X ] No" split_excel
-            case $split_excel in
-                c) echo "Oki"
+        echo "The MySQL service is not active. Do you want to turn on the service?"
+        read -p " [ C ] Accept | [ X ] Decline: " turnon_mysql
+        case $turnon_mysql in
+            [Cc])
+                echo "Attempting to start MySQL service..."
+                sudo systemctl start mysql
+                if systemctl is-active --quiet mysql; then
+                    echo "MySQL service started successfully."
+                else
+                    echo "Failed to start MySQL service."
+                fi
                 ;;
-                x) echo "Dummy"
+            [Xx])
+                echo "Exiting without starting MySQL service."
                 ;;
-            esac
+            *)
+                echo "Invalid option."
+                ;;
+        esac
     fi
 }
 
-mysql_on() {
-    database="$1"
+# Verify Database
+check_database() {
+    db_name="$1"
+    echo "Verifying if the database exists: $db_name"
 
-
+    # Comando para verificar la existencia de la base de datos
+    # Asegúrate de reemplazar las credenciales con las tuyas
+    if mysql -u "your_mysql_username" -p"your_mysql_password" -e "use $db_name;" 2>/dev/null; then
+        echo "The database '$db_name' is ready."
+    else
+        echo "The database '$db_name' wasn't found." 
+        echo "Do you want to create a database?"
+        read -p " [ C ] Yes | [ X ] No: " split_excel
+        case $split_excel in
+            [Cc])
+                echo "Creating database '$db_name'..."
+                mysql -u "your_mysql_username" -p"your_mysql_password" -e "CREATE DATABASE $db_name;"
+                ;;
+            [Xx])
+                echo "Exiting without creating database."
+                ;;
+            *)
+                echo "Invalid option."
+                ;;
+        esac
+    fi
 }
 
 clear
@@ -68,7 +94,7 @@ echo "                         | '---'K|| '---'R|| '---'O|| '---'O|| '---'N|    
 echo "                         '-------''-------''-------''-------''-------'    '-------''-------' "
 echo "                                    🔍 Instagram   Posts  and   Stories 🔍      "
 echo "                         ____________________________________________________________________"					
-echo "                               ︻デ═一   Created by: Ivenaccip  v.Alpha α ︻デ═一 " 
+echo "                               ︻デ═一  Created by: Ivenaccip  v.Alpha α  一═デ︻ " 
 echo "          ---------------------------------------------------------------------------------------------"
 echo "                      Automatisering van kunstmatige intelligentie van 'Kroon Op Het Werk'"
 echo "          ---------------------------------------------------------------------------------------------"
@@ -99,17 +125,18 @@ do
                 echo "                         '-------''-------''-------''-------''-------'    '-------''-------' "
                 echo "                                    🔍 Instagram   Posts  and   Stories 🔍      "
                 echo "                         ____________________________________________________________________"					
-                echo "                               ︻デ═一   Created by: Ivenaccip  v.Alpha α ︻デ═一 " 
+                echo "                               ︻デ═一  Created by: Ivenaccip  v.Alpha α  一═デ︻ " 
                 echo "          ---------------------------------------------------------------------------------------------"
                 echo "                      Automatisering van kunstmatige intelligentie van 'Kroon Op Het Werk'"
                 echo "          ---------------------------------------------------------------------------------------------"
                 echo
                 show_boxes
                 read -p "Select a file: " file_number
-                if [[ $file_number -ge 1 ]] && [[ $file_number-le 5 ]]; then
+                if [[ $file_number -ge 1 ]] && [[ $file_number -le 5 ]]; then
                     if [ $file_number -le $num_files ]; then
                         file_name="${files[$file_number-1]##*/}"
                         db_name="${file_name%.*}"  # Extrae el nombre base del archivo
+                        mysql_on
                         check_database "$db_name"
                     else
                         echo "Invalid selection."
